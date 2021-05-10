ae5757daa2caa96ce6e8b3f97e9b26f1c2b7df69
Rodolfo_Araujo_Vanucchi <rodolfo_vanucchi@hotmail.com>
2021-05-10 17:59:26 -0300

    Commit 2

--- app/src/main/java/br/com/brq/projeto_carteira_virtutal/MainActivity.kt
+++ app/src/main/java/br/com/brq/projeto_carteira_virtutal/MainActivity.kt
@@ -1,11 +1,34 @@
 package br.com.brq.projeto_carteira_virtutal
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.ContactsContract
+import android.widget.Button
+import android.widget.TextView
+import br.com.brq.projeto_carteira_virtutal.Telas.SegundaTela
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var botao: Button
+    lateinit var email: TextView
+    lateinit var senha: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        carregarElementos()
+
+        botao.setOnClickListener(){
+            startActivity(Intent(this, SegundaTela::class.java))
+        }
+
+    }
+
+    fun carregarElementos() {
+        email = findViewById<TextView>(R.id.loguin)
+        senha = findViewById<TextView>(R.id.senha)
+        botao = findViewById<Button>(R.id.botao)
     }
 }
---
+++ app/src/main/java/br/com/brq/projeto_carteira_virtutal/Telas/SegundaTela.kt
@@ -0,0 +1,12 @@
+package br.com.brq.projeto_carteira_virtutal.Telas
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.com.brq.projeto_carteira_virtutal.R
+
+class SegundaTela : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_segunda_tela)
+    }
+}
