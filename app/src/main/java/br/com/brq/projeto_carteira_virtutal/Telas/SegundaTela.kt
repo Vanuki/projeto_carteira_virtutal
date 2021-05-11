7e252108aa0a0e514fdc089823791690eca425d8
andressa <andressavieira7@hotmail.com>
2021-05-11 14:35:58 -0300

    Acrescentando strings e alteracoes no layout da main activity

--- app/src/main/java/br/com/brq/projeto_carteira_virtutal/Telas/SegundaTela.kt
+++ app/src/main/java/br/com/brq/projeto_carteira_virtutal/Telas/SegundaTela.kt
@@ -8,5 +8,6 @@ class SegundaTela : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_segunda_tela)
+
     }
 }
