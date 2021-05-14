5a89a0b2d3014658419b2bda44f54d428df493cb
Rodolfo_Araujo_Vanucchi <rodolfo_vanucchi@hotmail.com>
2021-05-14 11:42:33 -0300

    Adição de Códigos para Mostrar o Saldo(Incompleto)

---
+++ app/src/main/java/br/com/brq/projeto_carteira_virtutal/Model/Saldo.kt
@@ -0,0 +1,6 @@
+package br.com.brq.projeto_carteira_virtutal.Model
+
+import java.io.Serializable
+
+class Saldo(val valor: Double): Serializable {
+}
--- app/src/main/java/br/com/brq/projeto_carteira_virtutal/Telas/SegundaTela.kt
+++ app/src/main/java/br/com/brq/projeto_carteira_virtutal/Telas/SegundaTela.kt
@@ -3,21 +3,24 @@ package br.com.brq.projeto_carteira_virtutal.Telas
 
 import android.os.Bundle
 import android.widget.EditText
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import br.com.brq.projeto_carteira_virtutal.Adapter
 import br.com.brq.projeto_carteira_virtutal.Model.Gastos
+import br.com.brq.projeto_carteira_virtutal.Model.Saldo
 import br.com.brq.projeto_carteira_virtutal.R
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 
 class SegundaTela : AppCompatActivity(){
 
-
     lateinit var botaoSaldoAdd: FloatingActionButton
     lateinit var botaoGastosAdd: FloatingActionButton
-    lateinit var valor: EditText
+    lateinit var valorGasto: EditText
+    lateinit var valorSaldo: EditText
+    lateinit var somaSaldo: TextView
     lateinit var recycler: RecyclerView
     lateinit var adapter: Adapter
     lateinit var listaDeGastos: ArrayList<Gastos>
@@ -42,18 +45,24 @@ class SegundaTela : AppCompatActivity(){
 
     fun carregarEventos() {
         botaoGastosAdd.setOnClickListener() {
-            if(valor == null){
-                valor.error = "ERRO"
+            if(valorGasto == null){
+                valorGasto.error = "ERRO"
             }
             else{
-                adapter.addItem(Gastos(valor.text.toString().toDouble()))
+                adapter.addItem(Gastos(valorGasto.text.toString().toDouble()))
             }
+        }
+        botaoSaldoAdd.setOnClickListener() {
+            val saldo = Saldo(valorSaldo.toString().toDouble())
+            somaSaldo.text = "exemplo"
         }
     }
 
     fun carregarElementos() {
-        valor = findViewById(R.id.novo_gasto)
+        valorGasto = findViewById(R.id.novo_gasto)
+        valorSaldo = findViewById(R.id.novo_saldo)
         botaoSaldoAdd = findViewById(R.id.botaoSaldoAdd)
+        somaSaldo = findViewById(R.id.soma_saldo)
         botaoGastosAdd = findViewById(R.id.botaoGastosAdd)
         recycler = findViewById(R.id.gastos)
     }
