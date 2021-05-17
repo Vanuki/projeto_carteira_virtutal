cd30c3124f59f9ce2891237424d1c8ead216f3ea
andressa <andressavieira7@hotmail.com>
2021-05-17 16:33:41 -0300

    mudanca de cor e inclusao do import R na activity de objetivo

--- app/src/main/java/br/com/brq/projeto_carteira_virtutal/Telas/ObjetivoActivity.kt
+++ app/src/main/java/br/com/brq/projeto_carteira_virtutal/Telas/ObjetivoActivity.kt
@@ -2,6 +2,7 @@ package br.com.brq.projeto_carteira_virtutal.Telas
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import br.com.brq.projeto_carteira_virtutal.R
 
 class ObjetivoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
