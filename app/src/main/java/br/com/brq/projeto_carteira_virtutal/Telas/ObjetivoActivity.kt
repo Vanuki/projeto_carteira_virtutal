816b01d6a0c3cd9a8eb61a312bad28233cf1f260
Rodolfo_Araujo_Vanucchi <rodolfo_vanucchi@hotmail.com>
2021-05-18 11:18:28 -0300

    Adição do Botão Cofrinho na Segunda Tela

--- app/src/main/java/br/com/brq/projeto_carteira_virtutal/Telas/ObjetivoActivity.kt
+++ app/src/main/java/br/com/brq/projeto_carteira_virtutal/Telas/ObjetivoActivity.kt
@@ -7,6 +7,6 @@ import br.com.brq.projeto_carteira_virtutal.R
 class ObjetivoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_objetivo)
+        setContentView(R.layout.activity_terceira_tela)
     }
 }
